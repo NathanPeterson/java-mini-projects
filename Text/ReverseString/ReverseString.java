36eab4b10b55096f5a1d27cabccdd4811bfb3863
Nathan Peterson <nalexpeterson@gmail.com>
2017-03-16 22:55:03 -0700

    Add Palindrome checker to Reverse String Project

--- Text/ReverseString/ReverseString.java
+++ Text/ReverseString/ReverseString.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
 
 class ReverseString{
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    String reverseWord = "";
 
-    System.out.print("Enter a word: ");
-    String word = input.nextLine();
+  Scanner input = new Scanner(System.in);
+  String reverseWord = "";
+  String word ="";
+
+  public ReverseString(){
+    System.out.print("\n\tEnter a word or sentence: ");
+    word = input.nextLine().toLowerCase();
     int wordLength = word.length();
 
     for (int i = wordLength -1; i >= 0; i--) {
       reverseWord += word.charAt(i);
     }
 
-    System.out.println("\nYour word in reverse is: " + reverseWord +"\n\n");
+    System.out.println("\n\tYour word in reverse is: " + reverseWord);
+    System.out.println("\tWas your word a Palindrome: " + checkPalindrome() +"\n\n");
+  }
 
+  public boolean checkPalindrome(){
+    //replaceAll is used in case your word is a sentence.
+    //Ex: "Mr Owl Ate My Metal Worm" is a palindrome.
+    return word.replaceAll(" ", "").equals(reverseWord.replaceAll(" ", ""));
   }
 }
--- Text/ReverseString/runReverseWord.bat
+++
@@ -1,6 +0,0 @@
-FOR /L %%A IN (1,1,200) DO (
-  @echo off
-  javac *.java
-  java ReverseString
-  pause
-)
